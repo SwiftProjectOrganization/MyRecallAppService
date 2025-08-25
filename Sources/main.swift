76b7c4c493d2777c414c15d2235b533c6ecfc649
Rob J Goedman <goedman@icloud.com>
2025-08-25 14:30:59 -0600

    Updated paths in api

--- Sources/main.swift
+++ Sources/main.swift
@@ -15,10 +15,10 @@ struct MyRecallAppServiceAPIImpl: APIProtocol {
     let topic = input.query.topic ?? "BioChemistry"
     let content = input.query.content ?? "No content!"
     //print((user, topic, content.count))
-
+    
     let fileManager = FileManager.default
     let documentsUrl = fileManager.urls(for: .documentDirectory,
-                                                in: .userDomainMask)[0] as NSURL
+                                        in: .userDomainMask)[0] as NSURL
     
     let dirUrl = documentsUrl.appendingPathComponent("MyRecallApp/Data" + "/" + user)
     var isDir: ObjCBool = false
@@ -40,8 +40,8 @@ struct MyRecallAppServiceAPIImpl: APIProtocol {
       let topicUrl = dirUrl?.appendingPathComponent(topic + ".json")
       do {
         try content.write(to: topicUrl! as URL,
-                           atomically: true,
-                           encoding: String.Encoding.utf8)
+                          atomically: true,
+                          encoding: String.Encoding.utf8)
         let retcode = Components.Schemas.Json(message: "Wrote `\(topic)` for user `\(user)`!")
         return .ok(.init(body: .json(retcode)))
       } catch {
@@ -55,6 +55,66 @@ struct MyRecallAppServiceAPIImpl: APIProtocol {
       let retcode = Components.Schemas.Json(message: "Topic `\(topic)` for user `\(user)` not stored!")
       return .ok(.init(body: .json(retcode)))
     }
+  }
+  
+  func listTopics(_ input: Operations.ListTopics.Input) async throws -> Operations.ListTopics.Output {
+    let user = input.query.user ?? "Rob"
+    
+    let fileManager = FileManager.default
+    let documentsUrl = fileManager.urls(for: .documentDirectory,
+                                        in: .userDomainMask)[0] as NSURL
+    
+    let dirUrl = documentsUrl.appendingPathComponent("MyRecallApp/Data" + "/" + user)
+    var isDir: ObjCBool = false
+    if fileManager.fileExists(atPath: dirUrl!.path , isDirectory: &isDir) {
+      do {
+        let items = try fileManager.contentsOfDirectory(atPath: dirUrl!.path)
+        print(items)
+        let retcode = Components.Schemas.Json(message: "Reading topics for user `\(user)`!")
+        return .ok(.init(body: .json(retcode)))
+      } catch {
+        print(error.localizedDescription)
+        print("Could not read contents of directory \(String(describing: dirUrl!)).")
+        let retcode = Components.Schemas.Json(message: "Can't read topics for user `\(user)`")
+        return .ok(.init(body: .json(retcode)))
+      }
+    } else {
+      print("Given DIRECTORY \(String(describing: dirUrl!)) does not exist, nothing read.")
+      let retcode = Components.Schemas.Json(message: "Topics for user `\(user)` not read!")
+      return .ok(.init(body: .json(retcode)))
+    }
+    
+  }
+  
+  func getJson(_ input: Operations.GetJson.Input) async throws -> Operations.GetJson.Output {
+    let user = input.query.user ?? "Rob"
+    let topic = input.query.topic ?? "Huberman"
+    
+    let fileManager = FileManager.default
+    let documentsUrl = fileManager.urls(for: .documentDirectory,
+                                      in: .userDomainMask)[0] as NSURL
+    
+    let fileName = "MyRecallApp/Data" + "/" + user + "/" + topic + ".json"
+    let dirUrl = documentsUrl.appendingPathComponent(fileName)
+    var isDir: ObjCBool = false
+    if fileManager.fileExists(atPath: dirUrl!.path , isDirectory: &isDir) {
+      do {
+        let contents = try String(contentsOf: dirUrl!)
+        print(contents)
+        let retcode = Components.Schemas.Json(message: "Reading topic `\(topic)` for user `\(user)`!")
+        return .ok(.init(body: .json(retcode)))
+      } catch {
+        print(error.localizedDescription)
+        print("Could not read \(fileName).")
+        let retcode = Components.Schemas.Json(message: "Can't read topics for user `\(user)`")
+        return .ok(.init(body: .json(retcode)))
+      }
+    } else {
+      print("Given topic \(String(describing: dirUrl!)) does not exist, nothing read.")
+      let retcode = Components.Schemas.Json(message: "Topic `\(topic)` for user `\(user)` not read!")
+      return .ok(.init(body: .json(retcode)))
+    }
+
   }
 }
 
